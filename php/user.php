73c7137c48a4a65ed3e60aecbe5ecd473cd5106d
gianlucamb9 <gianlucamb9@gmail.com>
2024-01-13 11:03:37 -0800

    Small changes

--- php/user.php
+++ php/user.php
@@ -3,4 +3,16 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
 $conn = new mysqli($dbServer, $dbUser, $dbPass,$dbName);
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(isset($_SERVER["PATH_INFO"])){
+    switch($_SERVER["PATH_INFO"]){
+
+        case "/addbook":
+            break;
+
+        }
+    }
+}
+
 ?>
